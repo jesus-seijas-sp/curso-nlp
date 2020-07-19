9c044afd3dd54c8d3250944f4bb4bbc9bc218834
Jesus Seijas <jesus.seijas@axa.com>
2020-07-19 15:14:39 +0200

    feat: clasificador nlpjs

---
+++ 42-clasificador_nlpjs/index.js
@@ -0,0 +1,56 @@
+const { TokenizerEs } = require('@nlpjs/lang-es');
+const { NeuralNetwork } = require('@nlpjs/neural');
+const corpus = require('./corpus-es.json');
+
+const tokenizer = new TokenizerEs();
+
+function arrToObject(arr) {
+  const result = {};
+  arr.forEach(word => {
+    result[word] = 1;
+  });
+  return result;
+}
+
+function formatUtterance(utterance, intent) {
+  return {
+    input: arrToObject(tokenizer.tokenize(utterance, true)),
+    output: arrToObject([intent])
+  }
+}
+
+function buildTrainData() {
+  const result = [];
+  corpus.data.forEach(item => {
+    item.utterances.forEach(utterance => {
+      result.push(formatUtterance(utterance, item.intent));
+    });
+  });
+  return result;
+}
+
+function formatOutput(output) {
+  const result = [];
+  Object.keys(output).forEach(key => {
+    result.push({ label: key, score: output[key] });
+  });
+  return result.sort((a, b) => b.score - a.score);
+}
+
+const trainData = buildTrainData();
+const net = new NeuralNetwork({ log: true });
+net.train(trainData);
+let total = 0;
+let good = 0;
+corpus.data.forEach(item => {
+  item.tests.forEach(test => {
+    const validationData = formatUtterance(test, item.intent);
+    const output = net.run(validationData.input);
+    const classifications = formatOutput(output);
+    total += 1;
+    if (classifications[0].label === item.intent) {
+      good += 1;
+    }
+  });
+});
+console.log(`${good} good of a total of ${total} (${good * 100 / total}%)`);
